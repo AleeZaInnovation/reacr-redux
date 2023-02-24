2e72b30ca6b7146ca407e25d498034a396454bb8
theamik <118734316+theamik@users.noreply.github.com>
2023-02-24 19:46:24 +0600

    Update Design

--- src/components/Home.js
+++ src/components/Home.js
@@ -8,7 +8,16 @@ function Home() {
       <h1>Home Component</h1>
       <div className="cart-wrapper">
         <div className="img-wrapper item">
-          <img src="https://www.freeiconspng.com/img/45228.png"/>
+          <img src="https://www.dropbox.com/home?preview=9781119083931.jpg" />
+        </div>
+        <div className='text-wrapper item'>
+          <span>
+            I Phone
+          </span><br></br>
+          <span>Price: 100000</span>
+        </div>
+        <div className='btn-wrapper item'>
+          <button> Add to cart</button>
         </div>
       </div>
     </div>
