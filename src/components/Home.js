945a483aef48289ebdbdc675d4e5feeb55a2ed72
theamik <118734316+theamik@users.noreply.github.com>
2023-02-24 12:35:52 +0600

    Update Home

--- src/components/Home.js
+++ src/components/Home.js
@@ -6,7 +6,11 @@ function Home() {
   return (
     <div className="Home">
       <h1>Home Component</h1>
-
+      <div className="cart-wrapper">
+        <div className="img-wrapper item">
+          <img src="https://www.freeiconspng.com/img/45228.png"/>
+        </div>
+      </div>
     </div>
 
   );
